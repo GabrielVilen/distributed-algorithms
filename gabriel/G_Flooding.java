2174d57bc1a1119c6163742f7f8b46ede8341def
gabbe1993 <gabriel.vilen@gmail.com>
2016-11-15 13:34:52 +0100

    Updated flooding

--- gabriel/G_Flooding.java
+++ gabriel/G_Flooding.java
@@ -2,47 +2,21 @@ import teachnet.algorithm.BasicAlgorithm;
 
 import java.awt.Color;
 import java.util.Random;
-/**
- I: {NOT informed} // Executed by Initiator
-		SEND Explorer TO all Neighbors;
-		informed := TRUE;
 
-{Explorer from neighbor N is received}
-	IF NOT informed THEN
-		SEND Explorer TO all Neighbors except N;
-		informed := TRUE;
-		A := N;
-	ELSE
-		SEND Confirmation TO N;
-	FI
-	
-{Confirmation is received}
-	Count := Count + 1;
-	IF (NOT Initiator) AND (Count == #Neighbors – 1) THEN
-		SEND Confirmation TO Neighbor A;
-	FI
-	IF Initiator AND (Count == #Neighbors) THEN
-		Exit; // Algorithm is terminated.
-	FI
- 
- */
 public class G_Flooding extends BasicAlgorithm {
-	
+
 	boolean isInitiator = false, isInformed = false;
 	String confirmation = "Confirmation", explorer = "Explorer";
 	String caption;
 	Random rand;
 	Color color;
-	int count = 0;
-	private int a;
+	int count = 0,
+	int a;
 
 	@Override
 	public void setup(java.util.Map<String, Object> config) {
 		int id = (int) config.get("node.id");
-		if (id == 0) {
-			isInitiator = true;
-			//color = Color.YELLOW;
-		}
+		isInitiator = true;
 		color = Color.RED;
 		caption = "" + id;
 	}
@@ -58,27 +32,35 @@ public class G_Flooding extends BasicAlgorithm {
 		}
 
 	}
-	
+
 	private void inform() {
-		System.out.println(id + " is informed!");
-		isInformed = true;
+		a = n;
 		color = Color.GREEN;
-		
+
+		send(a, confirmation);
 	}
 
+	// TODO: idea: if higher seq (id) number found, update this seq nr and flood
+	// (= new msg found).
+	// else don't flood (already flooded msg)
 	@Override
 	public void receive(int n, Object message) {
 		// {Explorer from neighbor N is received}
 		if ((String) message == explorer) {
 
-			if (!isInformed) {
+			if (a != n) {				
 				for (int i = 0; i < checkInterfaces(); i++)
 					if (i != n) {
 						send(i, explorer);
 					}
-				inform();
+				// inform();
 				a = n;
-			} else {
+			}
+//			else if(!isInformed && checkInterfaces() == 1) { // leaf
+//				a = n;
+//			}
+
+			else {
 				send(n, confirmation);
 			}
 		}
@@ -86,13 +68,14 @@ public class G_Flooding extends BasicAlgorithm {
 		// {Confirmation is received}
 		if ((String) message == confirmation) {
 			count++;
+			System.out.println("ID: " + id + " count: " + count + " interfaces: " + checkInterfaces());
 
 			if (!isInitiator && (count == checkInterfaces() - 1)) {
-				send(a, confirmation);
-				//inform();
-			} 
+				//send(a, confirmation);
+				inform(a);
+			}
 			if (isInitiator && (count == checkInterfaces())) {
-				inform();				
+				inform();
 				exit();
 			}
 		}
