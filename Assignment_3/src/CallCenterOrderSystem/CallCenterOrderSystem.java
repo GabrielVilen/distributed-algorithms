bfaf472c2473fdd4c937d9bc9439954bfe7bcb7d
gabbe1993 <gabriel.vilen@gmail.com>
2017-01-12 10:53:17 +0100

    init assignment 3
    add maven with camel
    
    template for the 5 systems
    
    fixed broken pom
    
    add activeMQ example and Order class
    
    added description for running activemq

---
+++ Assignment_3/src/CallCenterOrderSystem/CallCenterOrderSystem.java
@@ -0,0 +1,7 @@
+package CallCenterOrderSystem;
+
+/**
+ * Created by gabri on 2017-01-12.
+ */
+public class CallCenterOrderSystem {
+}
---
+++ Assignment_3/src/util/HelloActiveMQ.java
@@ -0,0 +1,137 @@
+package util;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+/**
+ * To run the example: download ActiveMQ from http://activemq.apache.org/activemq-5143-release.html
+ * and run it with apache-activemq-5.11.1\bin\> activemq start
+ * and change URL to yours.
+ * see guide: http://javahonk.com/activemq-hello-world/
+ */
+public class HelloActiveMQ {
+
+    private static final String URL = "tcp://Gabriel:61616"; // change to yours
+
+    public static void main(String[] args) throws Exception {
+        HelloWorldConsumer[] consumers = new HelloWorldConsumer[10];
+        HelloWorldProducer[] producers = new HelloWorldProducer[10];
+
+        for (int i = 0; i < producers.length; i++) {
+            producers[i] = new HelloWorldProducer();
+            Thread threadProducer = new Thread(producers[i]);
+            threadProducer.start();
+        }
+        for (int i = 0; i < consumers.length; i++) {
+            consumers[i] = new HelloWorldConsumer();
+            Thread threadProducer = new Thread(consumers[i]);
+            threadProducer.start();
+        }
+
+    }
+
+    public static class HelloWorldProducer implements Runnable {
+        public void run() {
+            try {
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+                        URL);
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                // Create a Session
+                Session session = connection.createSession(false,
+                        Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("DemoQueue");
+
+                // Create a MessageProducer from the Session to the Topic or
+                // Queue
+                MessageProducer producer = session.createProducer(destination);
+                producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+                // Create a messages
+                String text = "Hello world! From: "
+                        + Thread.currentThread().getName() + " : "
+                        + this.hashCode();
+                TextMessage message = session.createTextMessage(text);
+
+                // Tell the producer to send the message
+                System.out.println("Sent message: " + message.hashCode()
+                        + " : " + Thread.currentThread().getName());
+                producer.send(message);
+
+                // Clean up
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class HelloWorldConsumer implements Runnable,
+            ExceptionListener {
+        public void run() {
+            try {
+
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+                        URL);
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                connection.setExceptionListener(this);
+
+                // Create a Session
+                Session session = connection.createSession(false,
+                        Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("DemoQueue");
+
+                // Create a MessageConsumer from the Session to the Topic or
+                // Queue
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                // Wait for a message
+                Message message = consumer.receive(1000);
+
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    String text = textMessage.getText();
+                    System.out.println("Received: " + text);
+                } else {
+                    System.out.println("Received: " + message);
+                }
+
+                consumer.close();
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        public synchronized void onException(JMSException ex) {
+            System.out.println("JMS Exception occured.  Shutting down client.");
+        }
+    }
+}
