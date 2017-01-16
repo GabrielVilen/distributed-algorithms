66f585f231a53e690cb512761d922ec8badffee6
gabbe1993 <gabriel.vilen@gmail.com>
2017-01-16 21:27:00 +0100

    Started CallCenterOrderSystem, not done yet (java.nio.file.AccessDeniedException)

--- EAI_Patterns_ApacheCamel/src/CallCenterOrderSystem/CallCenterOrderSystem.java
+++ EAI_Patterns_ApacheCamel/src/CallCenterOrderSystem/CallCenterOrderSystem.java
@@ -1,6 +1,21 @@
 package CallCenterOrderSystem;
 
+import Order.Order;
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * uses Channel Adapter 
+ * <p>
  * the Call center order system generates a text
  * file containing new orders every 2 minutes and stores it at a pre­defined
  * destination in the local file system. Each line represents a single order.
@@ -16,19 +31,37 @@ package CallCenterOrderSystem;
  */
 public class CallCenterOrderSystem {
 
-    public CallCenterOrderSystem() {
+    private final Path file;
+    private List<String> lines = new ArrayList<String>();
+
+    public CallCenterOrderSystem(Path file) {
+        this.file = file;
+
+        // creates new thread that writes to file
         new Thread(new Runnable() {
             public void run() {
                 try {
-                    // generate a new text file
-
-                    Thread.sleep(120000); // sleep 2 minutes
+                    Thread.sleep(1000); // sleep 2 minutes = 120000ms before write to file
+                    writeFile();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-        });
+        }).start();
     }
+
+    public void addOrder(Order order) {
+        String ordStr = order.getFirstName() + ", " + order.getLastName() + ", " + order.getNumberOfSurfboards()
+                + ", " + order.getNumberOfSurfboards() + ", " + order.getCustomerID();
+        lines.add(ordStr);
+    }
+
+    @SuppressWarnings("Since15")
+    private void writeFile() throws IOException {
+        Files.write(file, lines, Charset.forName("UTF-8")); // TODO: causes java.nio.file.AccessDeniedException:
+        System.out.println("Wrote line to file:  " + lines);
+    }
+
 }
 
 /**
@@ -43,6 +76,49 @@ public class CallCenterOrderSystem {
  * Start the context.
  */
 class Starter {
+
+    private static final String FILE_PATH = "order-file.txt";
+    private static final String TCP_LOCALHOST_61616 = "tcp://localhost:61616";
+    private static final String CC_NEW_ORDER = "activemq:queue:CC_NEW_ORDER";
+    private static final String NEW_ORDER = "activemq:queue:NEW_ORDER";
+
     public static void main(String[] args) {
+        try {
+            final Path file = Paths.get(FILE_PATH);
+            final CallCenterOrderSystem orderConsumer = new CallCenterOrderSystem(file);
+
+            // Create Camel Context
+            DefaultCamelContext camelContext = new DefaultCamelContext();
+
+            // Connect localhost ActiveMQ which should be separate process apache-activemq-5.14.3/bin$ ./activemq console
+            ActiveMQComponent activeMQComponent = ActiveMQComponent.activeMQComponent(TCP_LOCALHOST_61616);
+            camelContext.addComponent("activemq", activeMQComponent);
+            camelContext.addRoutes(new RouteBuilder(camelContext) {
+                @Override
+                public void configure() throws Exception {
+                    fromF("file://" +  FILE_PATH).to(NEW_ORDER); // create endpoint from file
+                }
+            });
+            camelContext.start();
+
+            testSystem(orderConsumer);
+
+            System.in.read(); // wait till ENTER pressed
+
+            camelContext.stop();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testSystem(CallCenterOrderSystem orderSystem) throws Exception {
+
+        for (int i = 0; i < 10; i++) {
+            Order order = new Order();
+            order.setFirstName("Alice_" + i);
+            order.setLastName("test_" + i);
+            orderSystem.addOrder(order);
+        }
     }
 }
